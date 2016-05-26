d2e22600bf43a695320aa819604e14d6a14e78f5
austinhays <ahays.target@yahoo.com>
2016-05-26 11:43:07 -0400

    Forum - refactor

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -7,33 +7,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static ArrayList<Post> parsePosts(filename) throws FileNotFoundException {
         ArrayList<Post> posts = new ArrayList<>();
-
-        // parse file
-        File f = new File("posts.txt");
+        File f = new File(filename);
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
             String[] columns = line.split("\\|");
             Post post = new Post(Integer.valueOf(columns[0]), columns[1], columns[2]);
             posts.add(post);
+            return posts;
         }
+    }
+
+    public static void printPosts(ArrayList<Post> posts, int currentPost) {
+        int postId = 0;
+        for (Post post : posts) {
+            if (post.replyId == currentPost) {
+                System.out.printf("[%s.] %s by %s\n", postId, post.text, post.author);
+            }
+
+            postId++;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<Post> posts = parsePosts("posts.txt");
+
 
         // start loop
         Scanner consoleScanner = new Scanner(System.in);
         int currentPost = -1;
         while (true) {
-            int postId = 0;
-            for (Post post : posts) {
-                if (post.replyId == currentPost) {
-                    System.out.printf("[%s.] %s by %s\n",postId, post.text, post.author);
-                }
-                System.out.printf("Current post: %s\n", currentPost);
-                System.out.println("Type the id you want to see replies to.");
-                currentPost = Integer.valueOf(consoleScanner.nextLine());
-                postId++;
+            printPosts(posts, currentPost);
+
             }
+            System.out.printf("Current post: %s\n", currentPost);
+            System.out.println("Type the id you want to see replies to.");
+            currentPost = Integer.valueOf(consoleScanner.nextLine());
         }
     }
 }
